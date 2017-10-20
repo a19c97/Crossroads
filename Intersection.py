26166d37539058061aec5008b822aaac6e19e3ff
AsicChen <a19c97@hotmail.com>
2017-10-19 23:16:17 -0400

    starting out

---
+++ Intersection.py
@@ -0,0 +1,11 @@
+# The game board that contains the intersection map, as well as all the cars and pedestrians
+
+class Intersection:
+
+    def __init__(self):
+        # Initialize 1000 by 1000 game board
+        
+        pass
+
+if __name__ == '__main__':
+    pass
---
+++ Simulation.py
@@ -0,0 +1,15 @@
+# Main method where the entire simulation runs
+
+class Simulation:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Simulation started:")
+
+if __name__ == '__main__':
+
+    # Initialize and run simulation
+    sim = Simulation()
+    sim.run()
